41e7749574a0628f0b9b0dcf5098b6d5675fa9c6
Vireak240406 <reak.vi2404@gmail.com>
2026-05-17 23:02:29 +0700

    challenge and practice W2

---
+++ challenge_W2.dart
@@ -0,0 +1,147 @@
+// //challenge1
+// class Point {
+//   int x = 0;
+//   int y = 0;
+
+//   Point({required this.x, required this.y}){}
+
+//   void translate(int deltaX,int deltaY){
+//     x+=deltaX;
+//     y+=deltaY;
+//     print("Result of delta is (${x},${y})");
+//   }
+//   String toString(){
+//     return"Result:${x},${y}";
+//   }
+// }
+// void main() {
+//   Point p1 = Point(x: 10, y: 20);
+//   print(p1);
+
+//   p1.translate(1,2);
+//   print(p1);
+
+// }
+
+// //challenge2
+// class Retangle {
+//   Point topLeft = Point(x: 0, y: 0);
+//   Point bottomRight = Point(x: 0, y: 0);
+//   int get getHeight => bottomRight.y - topLeft.y;
+//   int get getWidth => bottomRight.x - topLeft.x;
+//   int get getArea => getHeight * getWidth;
+
+//   Retangle({required this.bottomRight, required this.topLeft}){}
+
+//   String toString(){
+//     return "Width:$getWidth,Height:$getHeight,Area:$getArea";
+//   }
+// }
+
+// class Point {
+//   int x = 0;
+//   int y = 0;
+//   Point({required this.x, required this.y}){}
+
+// }
+
+// void main() {
+//   Retangle r1=Retangle(bottomRight: Point(x: 7, y: 9),topLeft: Point(x: 2, y: 3));
+//   print(r1);
+// }
+
+// //challenge3
+// class Person{
+//   String firstName;
+//   String lastName;
+
+//   Person({required this.firstName, required this.lastName,}){}
+
+//   String toString(){
+//     return"FullName:$firstName$lastName";
+//   }
+// }
+
+// void main(){
+//   Person p1=Person(firstName:'Oun', lastName: 'Vireak');
+//   print(p1);
+// }
+
+// //challenge4
+// class Point {
+//   final int x;
+//   final int y;
+
+//   Point({required this.x, required this.y}){}
+
+//   Point translate(int deltaX,int deltaY){
+//     return Point(x: deltaX+x, y: deltaY+y);
+//   }
+//   String toString(){
+//     return"Result:${x},${y}";
+//   }
+// }
+// void main() {
+//   Point p1 = Point(x: 10, y: 20);
+//   print(p1);
+
+//   p1=p1.translate(1,2);
+//   print(p1);
+
+// }
+
+// //challenge5
+// class TravelTicket {
+//   final String passenger;
+//   final String destination;
+//   final TravelClass travelClass;
+//   final String? discount;
+
+//   TravelTicket({
+//     required this.passenger,
+//     required this.destination,
+//     required this.travelClass,
+//     this.discount,
+//   }) {}
+
+//   String toString() {
+//     if (discount != null) {
+//       return "Ticket:$passenger -> $destination ,$travelClass,$discount";
+//     } else {
+//       return "Ticket:$passenger -> $destination ,$travelClass";
+//     }
+//   }
+// }
+
+// enum TravelClass { economy, business, first }
+
+// void main() {
+//   TravelTicket t1 = TravelTicket(
+//     passenger: "Vireak",
+//     destination: "Cambodai",
+//     travelClass: TravelClass.business,
+//     discount: "save20%",
+//   );
+//   print(t1);
+
+//   TravelTicket t2 = TravelTicket(
+//     passenger: "Vireak",
+//     destination: "Cambodai",
+//     travelClass: TravelClass.business,
+//     discount: "",
+//   );
+//   print(t2);
+// }
+
+//challenge6
+class Distance{
+    final double x;
+
+    Distance({required this.x}){}
+
+    
+
+}
+
+// Practice1
+
